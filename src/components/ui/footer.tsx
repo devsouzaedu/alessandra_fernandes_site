d570e2320a92b72d621bbc7a3bf993b2529458be
José Eduardo <dev.souzaedu@gmail.com>
2025-04-11 10:00:04 -0300

    v4

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { MapPin, Mail, Phone, Instagram, Linkedin } from "lucide-react";
+import { MapPin, Mail, Instagram } from "lucide-react";
 
 export function Footer() {
   return (
