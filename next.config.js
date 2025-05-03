31fd195dfc1b2ec9d2bac8af2294dcdf755f73c7
José Eduardo <dev.souzaedu@gmail.com>
2025-05-03 11:43:52 -0300

    vaii

--- next.config.js
+++ next.config.js
@@ -3,10 +3,36 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ['image/avif', 'image/webp'],
-    // Adicione domínios externos se necessário
-    // domains: ['exemplo.com'],
+    domains: [
+      'images.unsplash.com',
+      'source.unsplash.com',
+      'plus.unsplash.com',
+      'picsum.photos',
+      'imgur.com',
+      'i.imgur.com',
+      'firebasestorage.googleapis.com',
+      'storage.googleapis.com',
+      'lh3.googleusercontent.com',
+      'res.cloudinary.com',
+      'cloudinary.com',
+      'images.pexels.com',
+      'via.placeholder.com',
+      'placekitten.com',
+      'placehold.co',
+      'placeimg.com',
+      'loremflickr.com',
+      'amazonaws.com',
+      's3.amazonaws.com',
+      'myawsbucket.amazonaws.com'
+    ], // Permitindo domínios comuns para imagens
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**', // Permitir qualquer hostname HTTPS (use apenas em ambiente de desenvolvimento)
+      },
+    ],
   },
   // Configuração adicionais podem ser incluídas aqui
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
