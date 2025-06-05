27ba14a853ad718e812ebd39c3d26f3a915a1616
José Eduardo <dev.souzaedu@gmail.com>
2025-06-05 18:29:13 -0300

    seiko

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,6 +38,17 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17155066600"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17155066600');
+          `
+        }} />
+        
         {/* Preconnect com as fontes */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
